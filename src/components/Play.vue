bde102ff03dc68380b6563aafabcb365587b2aec
unknown <evievi@outlook.com>
2020-10-25 11:19:46 +0100

    Removed console.log

--- src/components/Play.vue
+++ src/components/Play.vue
@@ -1,122 +1,112 @@
 <template>
   <main>
-
     <header>
-      <p>Energy points : {{energy}}</p>
-      <p>Health points : {{health}}</p>
-      <p><button @click="restart()" class="restart"> Restart game </button></p>
+      <p>Energy points : {{ energy }}</p>
+      <p>Health points : {{ health }}</p>
+      <p><button @click="restart()" class="restart">Restart game</button></p>
     </header>
     <section class="all-cards">
-
-        <div class="foods" v-if="$store.state.foods.length>0">
-            <foodCard @clickCard='changeStats'/>
-            <foodCard @clickCard='changeStats'/>
-        </div>
-        <div class="trainings" v-if="$store.state.trainings.length>0">
-            <trainingCard @clickCard='changeStats'/>
-            <trainingCard @clickCard='changeStats'/>
-        </div>
-
+      <div class="foods" v-if="$store.state.foods.length > 0">
+        <foodCard @clickCard="changeStats" />
+        <foodCard @clickCard="changeStats" />
+      </div>
+      <div class="trainings" v-if="$store.state.trainings.length > 0">
+        <trainingCard @clickCard="changeStats" />
+        <trainingCard @clickCard="changeStats" />
+      </div>
     </section>
-    
-
   </main>
 </template>
 
 <script>
+  import foodCard from '../components/foodCard'
+  import trainingCard from '../components/trainingCard'
 
-import foodCard from '../components/foodCard'
-import trainingCard from '../components/trainingCard'
-
-export default {
-
-  computed: {
-      energy (){
-        return this.$store.state.energyPoints},
-      health () {
-        return this.$store.state.healthPoints},
-      counter () {
-        return this.$store.state.counter
+  export default {
+    computed: {
+      energy() {
+        return this.$store.state.energyPoints
       },
+      health() {
+        return this.$store.state.healthPoints
+      },
+      counter() {
+        return this.$store.state.counter
+      }
+    },
+    methods: {
+      changeStats: function(card) {
+        if (this.counter < 5) {
+          this.$store.commit('change', card)(this.$store.state.energyPoints)(
+            this.$store.state.healthPoints
+          )(this.$store.state.counter)
+        } else {
+          this.restart()
+        }
+      },
+      restart: function() {
+        this.$store.commit('restart')
+      }
+    },
 
-  },
-  methods : {
-      changeStats: function (card) { 
-  
-          if(this.counter < 5){
-              this.$store.commit('change', card) 
-              console.log(this.$store.state.energyPoints)
-              console.log(this.$store.state.healthPoints)
-              console.log(this.$store.state.counter)
-
-          } else {
-              this.restart()
-          }
-        }, 
-        restart: function () { 
-            this.$store.commit('restart')}
-},
-
-  name: 'Play',
-  components: {
-    foodCard,
-    trainingCard
+    name: 'Play',
+    components: {
+      foodCard,
+      trainingCard
+    }
   }
-    
-}
 </script>
 
 <style>
-    .food-card:hover, .training-card:hover{
-        cursor: pointer;
-    }
+  .food-card:hover,
+  .training-card:hover {
+    cursor: pointer;
+  }
 
-    .pic{
-      height: 8em
-    }
+  .pic {
+    height: 8em;
+  }
 </style>
 <style scoped>
-    header {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-        background-color: rgb(65, 224, 224);
-        color: rgb(54, 52, 52);
-        font-weight:bolder
-    }
+  header {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(65, 224, 224);
+    color: rgb(54, 52, 52);
+    font-weight: bolder;
+  }
 
-    header p {
-        padding: 0 1em;
-    }
-    .foods, .trainings{
-      display: grid;
-      grid-template-columns: auto auto auto;
-      column-gap: 1em;
-    }
+  header p {
+    padding: 0 1em;
+  }
+  .foods,
+  .trainings {
+    display: grid;
+    grid-template-columns: auto auto auto;
+    column-gap: 1em;
+  }
 
-    .foods{
-      border-right: 2px solid gray;
-    }
-    .all-cards{
-        margin: 1em 0;
-        display: grid;
-        grid-template-rows: auto;
-        grid-template-columns: 50% 50%;        
-        align-items: center;
-        justify-content: center;
-        height : 70vh
-    }
+  .foods {
+    border-right: 2px solid gray;
+  }
+  .all-cards {
+    margin: 1em 0;
+    display: grid;
+    grid-template-rows: auto;
+    grid-template-columns: 50% 50%;
+    align-items: center;
+    justify-content: center;
+    height: 70vh;
+  }
 
-  
-   
-   .restart{
-     border: 1px solid rgb(2, 2, 46);
-     border-radius: 0.5em;
-     padding: 0.5em;
-     background-color: white;
-     color: rgb(2, 2, 46);
-     cursor: pointer;
-
-   }
+  .restart {
+    border: 1px solid rgb(2, 2, 46);
+    border-radius: 0.5em;
+    padding: 0.5em;
+    background-color: white;
+    color: rgb(2, 2, 46);
+    cursor: pointer;
+  }
 </style>
--- src/components/trainingCard.vue
+++ src/components/trainingCard.vue
@@ -1,39 +1,43 @@
 <template>
-  <div v-if="trainingIndex>=0" class="training-card" @click="send(training)">
-    <h3>{{training.name}}</h3>
-    <p><img :src="'/images/' + training.pic " alt="training-pic" class="pic"></p>
+  <div v-if="trainingIndex >= 0" class="training-card" @click="send(training)">
+    <h3>{{ training.name }}</h3>
+    <p>
+      <img :src="'/images/' + training.pic" alt="training-pic" class="pic" />
+    </p>
     <p>Energy: {{ training.energy }}</p>
   </div>
 </template>
 
 <script>
-export default {
-  computed: {
-    
-    trainingIndex(){
-        if (this.$store.state.trainings.length > 0) { 
-          let index = Math.floor(Math.random() * this.$store.state.trainings.length)
-          return index} else { return null}
+  export default {
+    computed: {
+      trainingIndex() {
+        if (this.$store.state.trainings.length > 0) {
+          let index = Math.floor(
+            Math.random() * this.$store.state.trainings.length
+          )
+          return index
+        } else {
+          return null
+        }
       },
-      training(){
-        let card = this.$store.state.trainings.find(f => this.$store.state.trainings.indexOf(f) === this.trainingIndex)
-        return card;
-      },
-  },
-  methods : {
-    send(card){
-      if ( this.$store.state.energyPoints < Math.abs(card.energy) ){
-        console.log('You need to eat before you can train!')
-        return null
+      training() {
+        let card = this.$store.state.trainings.find(
+          (f) => this.$store.state.trainings.indexOf(f) === this.trainingIndex
+        )
+        return card
       }
-      else {
-      this.$emit('clickCard', card) 
+    },
+    methods: {
+      send(card) {
+        if (this.$store.state.energyPoints < Math.abs(card.energy)) {
+          ;('You need to eat before you can train!')
+          return null
+        } else {
+          this.$emit('clickCard', card)
+        }
       }
-      
-    }
-  },
-  name: 'trainingCard',
- 
- 
-}
+    },
+    name: 'trainingCard'
+  }
 </script>
